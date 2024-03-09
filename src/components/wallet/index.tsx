3b565b3d7d5425ddf6ac181723ebbef0ba9e84c3
patel7vishal <pv251023@gmail.com>
2024-03-09 16:52:36 +0530

    Added wallet connectivity

--- src/components/wallet/index.tsx
+++ src/components/wallet/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BiWallet } from 'react-icons/bi';
-
 import Button from '../common/button';
+import { WalletModalButton } from '@solana/wallet-adapter-react-ui';
 
 const Wallet = (): JSX.Element => {
-  return <Button icon={<BiWallet className='h-5 w-5' />}>Connect</Button>;
+  return <Button icon={<BiWallet className='h-5 w-5' />}>
+    <WalletModalButton>
+      Connect
+    </WalletModalButton>
+  </Button>;
 };
 
 export default Wallet;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,4 +3,6 @@ import '@/styles/colors.css';
 
 import Base from '@/components/common/base';
 import WithLoading from '@/components/common/withLoading';
-export default WithLoading(Base);
+import WithWalletAdapter from '@/components/web3/walletAdapterContext';
+require('@solana/wallet-adapter-react-ui/styles.css');
+export default WithWalletAdapter(WithLoading(Base))
